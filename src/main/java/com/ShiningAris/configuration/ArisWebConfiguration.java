5ab34bee5e452326c15346954311670041935445
Zehan Tian <45849694+Silky-Littleraisin@users.noreply.github.com>
2019-11-18 19:41:07 +0900

    Update and rename WendaWebConfiguration.java to ArisWebConfiguration.java

--- src/main/java/com/ShiningAris/configuration/ArisWebConfiguration.java
+++ src/main/java/com/ShiningAris/configuration/ArisWebConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Created by nowcoder on 2016/7/3.
  */
 @Component
-public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
+public class ArisWebConfiguration extends WebMvcConfigurerAdapter {
     @Autowired
     PassportInterceptor passportInterceptor;
 
